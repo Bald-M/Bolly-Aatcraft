be6f240c7b8194870399d6492acf95667e5162da
Lance <2411663991@qq.com>
2023-10-22 22:57:32 +1300

    "Add node.js"

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname)); // 以当前目录为静态资源目录
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
